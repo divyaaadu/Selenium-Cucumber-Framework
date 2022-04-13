d6f8a7205143124c77c3cd60d77878aff6362ea0
divyaaadu <divyaaadu@gmail.com>
2022-04-13 12:15:37 +0100

    commiting first project

---
+++ src/main/java/SearchItemNext.java
@@ -0,0 +1,20 @@
+
+public class SearchItemNext {
+
+
+
+
+    @test
+
+    public void SearchAnItemUsingProductName(){
+        WebdriverManager.chromedriver
+
+
+
+    }
+
+    public void SearchAnItemUsingBrandName(){
+
+    }
+
+}
